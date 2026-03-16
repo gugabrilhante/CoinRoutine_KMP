e0a0dc598517abbb60185bc0aab4de23261578d4
gustavobrilhante <gugabrilhante@gmail.com>
2026-03-16 17:26:30 -0300

    implement GetCoinsListUseCase

--- composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/coins/domain/GetCoinListUseCase.kt
+++ composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/coins/domain/GetCoinListUseCase.kt
@@ -1,2 +1,18 @@
 package com.gustavo.brilhante.coinroutine.coins.domain
 
+import com.gustavo.brilhante.coinroutine.coins.domain.api.CoinsRemoteDataSource
+import com.gustavo.brilhante.coinroutine.coins.domain.mapper.toCoinModel
+import com.gustavo.brilhante.coinroutine.coins.domain.model.CoinModel
+import com.gustavo.brilhante.coinroutine.core.domain.Result
+import com.gustavo.brilhante.coinroutine.core.domain.DataError
+import com.gustavo.brilhante.coinroutine.core.domain.map
+
+class GetCoinsListUseCase(
+    private val client: CoinsRemoteDataSource,
+) {
+    suspend fun execute(): Result<List<CoinModel>, DataError.Remote> {
+        return client.getListOfCoins().map { dto ->
+            dto.data.coins.map { it.toCoinModel() }
+        }
+    }
+}
