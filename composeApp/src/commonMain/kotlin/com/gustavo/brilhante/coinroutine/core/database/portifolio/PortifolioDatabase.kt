f19bed29f29504ed2fc56c62288934dc89a430e2
gustavobrilhante <gugabrilhante@gmail.com>
2026-03-17 18:04:45 -0300

    Add UserBalanceDao.kt

--- composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/core/database/portifolio/PortifolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/core/database/portifolio/PortifolioDatabase.kt
@@ -5,9 +5,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.gustavo.brilhante.coinroutine.portfolio.data.local.PortfolioCoinEntity
 import com.gustavo.brilhante.coinroutine.portfolio.data.local.PortfolioDao
+import com.gustavo.brilhante.coinroutine.portfolio.data.local.UserBalanceDao
+import com.gustavo.brilhante.coinroutine.portfolio.data.local.UserBalanceEntity
 
-@Database(entities = [PortfolioCoinEntity::class], version = 1)
+@Database(entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 @ConstructedBy(PortfolioDatabaseCreator::class)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
+    abstract fun userBalanceDao(): UserBalanceDao
 }
