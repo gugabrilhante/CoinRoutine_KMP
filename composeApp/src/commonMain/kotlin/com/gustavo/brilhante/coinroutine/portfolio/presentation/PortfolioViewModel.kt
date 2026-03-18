33658f44fdf2ca7bb04c89a0aa5af4da7fef7d34
gustavobrilhante <gugabrilhante@gmail.com>
2026-03-18 19:39:26 -0300

    Add PortfolioViewModelTest unit test

--- composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/portfolio/presentation/PortfolioViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/gustavo/brilhante/coinroutine/portfolio/presentation/PortfolioViewModel.kt
@@ -10,15 +10,19 @@ import com.gustavo.brilhante.coinroutine.core.util.formatPercentage
 import com.gustavo.brilhante.coinroutine.core.util.toUiText
 import com.gustavo.brilhante.coinroutine.portfolio.domain.PortfolioCoinModel
 import com.gustavo.brilhante.coinroutine.portfolio.domain.PortfolioRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
 class PortfolioViewModel(
-    private val portfolioRepository: PortfolioRepository
+    private val portfolioRepository: PortfolioRepository,
+    private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(PortfolioState(isLoading = true))
@@ -46,7 +50,7 @@ class PortfolioViewModel(
         }
     }.onStart {
         portfolioRepository.initializeBalance()
-    }.stateIn(
+    }.flowOn(coroutineDispatcher).stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(),
         initialValue = PortfolioState(isLoading = true)
